570a6f280e6c9df5d7137c29a998c0933a2e9402
MaurusGubser <maurus.gubser@gmx.ch>
2021-05-31 10:09:25 +0200

    Added test_size parameter to grid_search_model

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ data_parameters = OrderedDict([('read_image', read_image), ('read_hist', read_hi
                                ('nb_components_pca', nb_components_pca), ('batch_size_pca', batch_size_pca),
                                ('hist_hsl', hist_hsl), ('hist_h', hist_h), ('hist_s', hist_s), ('hist_l', hist_l),
                                ('percentage_true', percentage_true), ('with_mean', with_mean), ('with_std', with_std)])
-test_size = 0.2  # fraction of test set
+test_size = 0.15  # fraction of test set
 
 # ----- train and evaluate models -----
 train_models = False
