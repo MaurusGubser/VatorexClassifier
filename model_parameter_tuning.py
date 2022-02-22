62a35bd739ebdc86d3e6c44087d1c7b7b82b4c77
MaurusGubser <maurus.gubser@gmx.ch>
2022-02-22 15:28:34 +0100

    njobs default to -1

--- model_parameter_tuning.py
+++ model_parameter_tuning.py
@@ -21,7 +21,7 @@ def compute_cv_scores(model_type: object, data: np.ndarray, labels: np.ndarray, 
     k = cv_params['nb_split_cv']
     train_scores, test_scores = validation_curve(estimator=model_type, X=data, y=labels, param_name=model_parameter,
                                                  param_range=parameter_range, cv=k, scoring=score_param,
-                                                 n_jobs=4, verbose=1, fit_params=weights_dict)
+                                                 n_jobs=-1, verbose=1, fit_params=weights_dict)
     return train_scores, test_scores
 
 
