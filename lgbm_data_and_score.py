4f4ab1dd53f1a313d98903d70226e69b9b5dcaba
MaurusGubser <maurus.gubser@gmx.ch>
2022-03-21 13:49:36 +0100

    lgbm_data_and_score.py for comparision of LGBM between python and C

---
+++ lgbm_data_and_score.py
@@ -0,0 +1,39 @@
+import os.path
+
+import numpy as np
+import lightgbm as lgb
+
+from data_reading_writing import load_data_and_labels, concatenate_data
+
+
+# ---------------------- working dir ----------------------
+working_dir = 'LGBM_Comparison/Test_0_matching05_mindist015/'
+assert os.path.exists(working_dir), 'Directory {} does not exist.'.format(working_dir)
+
+# ---------------------- load data and labels ----------------------
+path_data = working_dir + 'data.npz'    # must be already stored as .npz, for convenience
+_, data_hist, labels, _ = load_data_and_labels(path_data)
+print('Data of shape {}'.format(data_hist.shape))
+
+# ---------------------- load model ----------------------
+path_model = working_dir + 'model.txt'
+model = lgb.Booster(model_file=path_model)
+
+# ---------------------- model prediction ----------------------
+pred_proba = model.predict(data_hist)
+
+# ---------------------- save data and prediction ----------------------
+
+export_path_data = working_dir + 'data.txt'
+with open(export_path_data, 'w') as out_data:
+    np.savetxt(out_data, data_hist)
+
+export_path_labels = working_dir + 'labels.txt'
+with open(export_path_labels, 'w') as out_labels:
+    np.savetxt(out_labels, labels)
+
+export_path_predictions = working_dir + 'pred_proba.txt'
+with open(export_path_predictions, 'w') as out_pred:
+    np.savetxt(out_pred, pred_proba)
+
+print('Saved data, labels and predictions.')
