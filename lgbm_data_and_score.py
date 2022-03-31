6ea4ad211819e37c9bf328938dbd80140eb8920f
MaurusGubser <maurus.gubser@gmx.ch>
2022-03-31 12:26:50 +0200

    Added export of predicted labels.

--- lgbm_data_and_score.py
+++ lgbm_data_and_score.py
@@ -1,9 +1,8 @@
 import os.path
-
 import numpy as np
 import lightgbm as lgb
 
-from data_reading_writing import load_data_and_labels, concatenate_data
+from data_reading_writing import load_data_and_labels
 
 
 # ---------------------- working dir ----------------------
@@ -21,6 +20,7 @@ model = lgb.Booster(model_file=path_model)
 
 # ---------------------- model prediction ----------------------
 pred_proba = model.predict(data_hist)
+pred_labels = np.around(pred_proba)
 
 # ---------------------- save data and prediction ----------------------
 
@@ -33,7 +33,11 @@ with open(export_path_labels, 'w') as out_labels:
     np.savetxt(out_labels, labels)
 
 export_path_predictions = working_dir + 'pred_proba.txt'
-with open(export_path_predictions, 'w') as out_pred:
-    np.savetxt(out_pred, pred_proba)
+with open(export_path_predictions, 'w') as out_pred_proba:
+    np.savetxt(out_pred_proba, pred_proba)
+
+export_path_pred_labels = working_dir + 'pred_labels.txt'
+with open(export_path_pred_labels, 'w') as out_pred_labels:
+    np.savetxt(out_pred_labels, pred_labels)
 
 print('Saved data, labels and predictions.')
