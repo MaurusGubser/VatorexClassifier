926411053dd9345dd262436676e51732081da5e9
MaurusGubser <maurus.gubser@gmx.ch>
2022-04-08 18:00:23 +0200

    Added (in comments) use of plot_learning_curve

--- roc_precrcll_curves.py
+++ roc_precrcll_curves.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import learning_curve
 from sklearn.metrics import RocCurveDisplay, PrecisionRecallDisplay
+from sklearn.svm import LinearSVC
+from collections import OrderedDict
 
 from data_reading_writing import read_data_and_labels_from_path
 from data_handling import split_and_sample_data
@@ -64,10 +66,11 @@ def plot_learning_curve(estimator: object, title: str, X: np.ndarray, y: np.ndar
 
 def plot_learning_curve_model(folder_path: str, data_params: dict, model: object, model_name: str) -> None:
     data, labels, paths = read_data_and_labels_from_path(folder_path, data_params)
-    data, labels, _ = split_and_sample_data(data=data,
-                                            labels=labels,
-                                            paths_imgs=paths,
-                                            test_size=None)
+    # X_train, X_test, y_train, y_test, paths_train, paths_test
+    data, _, labels, _, _, _ = split_and_sample_data(data=data,
+                                                     labels=labels,
+                                                     paths_imgs=paths,
+                                                     test_size=None)
     cv = 10
     plot_learning_curve(model, model_name, data, labels, ylim=None, cv=cv)
     return None
@@ -97,3 +100,37 @@ def plot_roc_precrcll_curves(clf: object, dir_data: str, data_params: dict, test
         figs.append(compute_metric_curve(metric, name, clf, X_test, y_test))
         plt.show()
     return figs
+
+"""
+# ------------------------ remove -------------------------
+read_image = False  # True or False
+read_hist = 'context'  # must be 'candidate', 'context' or None
+with_image = None  # must be None or a scalar, which defines downsize factor; use image
+with_binary_patterns = False  # use local binary patterns of image
+histogram_params = None  # (3, 16)  # must be None or a tuple of two integers, which describes (nb_divisions, nb_bins)
+nb_segments = None  # must be None or an integer; segment image using k-means in color space
+threshold_low_var = None  # must be None or a float in [0.0, 1.0], which defines threshold for minimal variance
+nb_components_pca = None  # must be None or an integer, which defines number of components
+batch_size_pca = None  # must be an integer, should be >= nb_features (ideally larger) and <= nb_images
+hist_hsl = True
+hist_h = True
+hist_s = True
+hist_l = True
+quadratic_features = False  # use basis 1, x_i, x_i**2, no mixed terms
+with_mean = False  # data gets shifted such that mean is 0.0
+with_std = False  # data gets scaled such that std is 1.0
+with_false1 = False  # use false1 labelled data
+
+path = 'Candidate_Images/Series_matching05_mindist015_test'
+params = OrderedDict([('read_image', read_image), ('read_hist', read_hist), ('with_image', with_image),
+                      ('with_binary_patterns', with_binary_patterns), ('histogram_params', histogram_params),
+                      ('nb_segments', nb_segments), ('threshold_low_var', threshold_low_var),
+                      ('nb_components_pca', nb_components_pca), ('batch_size_pca', batch_size_pca),
+                      ('hist_hsl', hist_hsl), ('hist_h', hist_h), ('hist_s', hist_s), ('hist_l', hist_l),
+                      ('quadratic_features', quadratic_features), ('with_mean', with_mean),
+                      ('with_std', with_std), ('with_false1', with_false1)])
+mdl = LinearSVC()
+name = 'LinearSVC'
+
+plot_learning_curve_model(folder_path=path, data_params=params, model=mdl, model_name=name)
+"""
