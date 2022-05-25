64174de12c6a26e22ded95c7beec3906dfa2c803
MaurusGubser <maurus.gubser@gmx.ch>
2022-05-25 16:50:57 +0200

    Removed plot roc and precision-recall curve.

--- main.py
+++ main.py
@@ -28,21 +28,21 @@ data_parameters = OrderedDict([('read_image', read_image), ('read_hist', read_hi
                                ('hist_hsl', hist_hsl), ('hist_h', hist_h), ('hist_s', hist_s), ('hist_l', hist_l),
                                ('with_mean', with_mean), ('with_std', with_std), ('with_false1', with_false1)])
 test_size = 0.10  # must be float in (0,1); fraction of test set
-path_image_folders = 'path/to/data'  # path to data
+path_image_folders = 'Candidate_Images/SmallSeries'  # path to data
 
 # ----- train and compare models -----
 compare_models = True
 
-log_reg = True
+log_reg = False
 sgd = False
 ridge_class = False
 decision_tree = False
-random_forest = True
+random_forest = False
 l_svm = True
 nl_svm = False
-naive_bayes = True
-ada_boost = True
-histogram_boost = True
+naive_bayes = False
+ada_boost = False
+histogram_boost = False
 gradient_boost = False
 
 model_selection = OrderedDict([('log_reg', log_reg), ('sgd', sgd), ('ridge_class', ridge_class),
@@ -54,11 +54,11 @@ model_selection = OrderedDict([('log_reg', log_reg), ('sgd', sgd), ('ridge_class
 # ----- cross-validation for one parameter -----
 cross_validation = False
 
-model_cv = LGBMClassifier(is_unbalanced='balanced')
+model_cv = LGBMClassifier(class_weight='balanced')
 model_name = 'LGBM_balanced'
 
 model_parameter = 'n_estimators'  # e.g. C, learning_rate, max_iter, max_depth, l2_regularization depending on model
-parameter_range = np.array([100, 200, 300, 400, 500])
+parameter_range = np.array([10, 20, 30, 40, 50])
 semilog = False  # if x-axis should be logarithmic
 nb_split_gs = 10  # number of split cvs
 
@@ -69,7 +69,7 @@ cv_parameters = OrderedDict([('model_name', model_name), ('model_parameter', mod
 # ----- grid search for several parameters -----
 grid_search = False
 
-model_gs = LGBMClassifier(is_unbalanced='balanced')
+model_gs = LGBMClassifier(class_weight='balanced')
 model_name = 'LGBM_balanced'
 scoring_parameters = ['recall', 'precision', 'f1', 'roc_auc']
 refit_param = 'f1'
@@ -86,14 +86,10 @@ gs_parameters = OrderedDict([('model_name', model_name), ('parameters_grid', par
                              ('scoring_parameters', scoring_parameters), ('refit_param', refit_param),
                              ('nb_split_cv', nb_split_cv)])
 
-# ----------- plot curves ----------------------
-plot_curves = False
-clf = LGBMClassifier()
-
 # ----- evaluate trained model ------
 evaluate_model = False
 path_trained_model = 'path/to/trained/model.txt'
-path_test_data = 'path/to/data/'
+path_test_data = 'path/to/data'
 model_name = 'data_model_name'
 
 # ----- train and export Vatorex model ------
@@ -120,8 +116,6 @@ if __name__ == '__main__':
         cross_validation_one_param(model_cv, path_image_folders, data_parameters, cv_parameters)
     elif grid_search:
         grid_search_multi_param(model_gs, path_image_folders, data_parameters, gs_parameters, test_size)
-    elif plot_curves:
-        plot_roc_precrcll_curves(clf, path_image_folders, data_parameters, test_size)
     elif evaluate_model:
         evaluate_trained_model(path_test_data, data_parameters, path_trained_model, model_name)
     elif train_export_Vatorex:
